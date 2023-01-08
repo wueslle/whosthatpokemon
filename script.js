db169e97d7e3389c65a0dc2601ad71addd0c9da0
wueslle <wuesllets@gmail.com>
2023-01-08 18:31:52 -0300

    correção imagens

--- script.js
+++ script.js
@@ -89,7 +89,7 @@ const pokeCheck = () => {
 
 const pokeErro = () => {
   const erro = document.createElement("img");
-  erro.src = "assets/pokeball.png";
+  erro.src = "whosthatpokemon/assets/pokeball.png";
   erro.id = "erro";
   erro.className = `erro${tentativas}`;
   document.getElementById("palpites").appendChild(erro);
